f0a97e7e7cb55b7f9e2a8d6a6f75b6e798d30b73
Murad Aliyev <muradaliev@hotmail.com>
2026-01-21 14:39:03 +0100

    fix in cli

--- src/sfobjects-to-typescript-cli.ts
+++ src/sfobjects-to-typescript-cli.ts
@@ -2,82 +2,80 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 import { exctract } from './sfobjects-to-typescript';
 
-async function run() {
+export async function run() {
 
-    if (typeof window === "undefined") {
+    const yarg = yargs(hideBin(process.argv))
 
-        const yarg = yargs(hideBin(process.argv))
+    const o = await yarg
+        .scriptName('sfobjects-to-typescript')
+        .option('username', {
+            describe: 'Salesforce username',
+            alias: 'u',
+            type: 'string',
+            demandOption: true
+        })
+        .option('password', {
+            describe: 'Salesforce password',
+            alias: 'p',
+            type: 'string',
+            demandOption: true
+        })
+        .option('objects', {
+            describe: 'List of objects to generate types for',
+            alias: 'obj',
+            type: 'string',
+            array: true,
+            demandOption: true
+        })
+        .option('login_url', {
+            describe: 'Salesforce login URL e.g. https://login.salesforce.com/',
+            alias: 'lurl',
+            type: 'string'
+        })
+        .option('server_url', {
+            describe: 'Salesforce SOAP service endpoint URL e.g. https://na1.salesforce.com/services/Soap/u/28.0',
+            alias: 'surl',
+            type: 'string'
+        })
+        .option('instance_url', {
+            describe: 'Salesforce instance URL e.g. https://na1.salesforce.com/',
+            alias: 'iurl',
+            type: 'string'
+        })
+        .option('client_id', {
+            describe: 'Salesforce client id',
+            alias: 'c',
+            type: 'string'
+        })
+        .option('client_secret', {
+            describe: 'Salesforce client secret',
+            alias: 's',
+            type: 'string'
+        })
 
-        const o = await yarg
-            .scriptName('sfobjects-to-typescript')
-            .option('username', {
-                describe: 'Salesforce username',
-                alias: 'u',
-                type: 'string',
-                demandOption: true
-            })
-            .option('password', {
-                describe: 'Salesforce password',
-                alias: 'p',
-                type: 'string',
-                demandOption: true
-            })
-            .option('objects', {
-                describe: 'List of objects to generate types for',
-                alias: 'obj',
-                type: 'string',
-                array: true,
-                demandOption: true
-            })
-            .option('login_url', {
-                describe: 'Salesforce login URL e.g. https://login.salesforce.com/',
-                alias: 'lurl',
-                type: 'string'
-            })
-            .option('server_url', {
-                describe: 'Salesforce SOAP service endpoint URL e.g. https://na1.salesforce.com/services/Soap/u/28.0',
-                alias: 'surl',
-                type: 'string'
-            })
-            .option('instance_url', {
-                describe: 'Salesforce instance URL e.g. https://na1.salesforce.com/',
-                alias: 'iurl',
-                type: 'string'
-            })
-            .option('client_id', {
-                describe: 'Salesforce client id',
-                alias: 'c',
-                type: 'string'
-            })
-            .option('client_secret', {
-                describe: 'Salesforce client secret',
-                alias: 's',
-                type: 'string'
-            })
+        .option('token', {
+            describe: 'Salesforce api token',
+            alias: 't',
+            type: 'string'
+        })
+        .option('access_token', {
+            describe: 'Salesforce OAuth2 access token',
+            alias: 'at',
+            type: 'string'
+        })
 
-            .option('token', {
-                describe: 'Salesforce api token',
-                alias: 't',
-                type: 'string'
-            })
-            .option('access_token', {
-                describe: 'Salesforce OAuth2 access token',
-                alias: 'at',
-                type: 'string'
-            })
+        .option('path', {
+            describe: 'The output folder, default is stdout',
+            alias: 'o',
+            type: 'string'
+        })
+        .help()
+        .usage("Usage: sfobjects-to-typescript --username <username> --password <password> --objects <object_1> <object _2> [more options]")
+        .parse();
 
-            .option('path', {
-                describe: 'The output folder, default is stdout',
-                alias: 'o',
-                type: 'string'
-            })
-            .help()
-            .usage("Usage: sfobjects-to-typescript --username <username> --password <password> --objects <object_1> <object _2> [more options]")
-            .parse();
-
-        await exctract(o);
-    }
+    await exctract(o);
 }
 
-run();
-
+if (typeof window === "undefined" && require.main === module) {
+    run();
+}
